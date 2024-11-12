83451ebc7453a8231ba16266c44b8f26ecde74b0
aab016 <prof.alessandro_aglietti@iislagrange.edu.it>
2024-11-12 22:17:06 +0000

    feat(java): oz digit

--- OhMyByteCode/src/App.java
+++ OhMyByteCode/src/App.java
@@ -1,21 +1,26 @@
-import java.lang.management.ManagementFactory;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-        System.out.printf("jvmName: %s%n", jvmName);
+        System.out.println("Ciao sono una slot machine FURBA!");
 
         long pid = ProcessHandle.current().pid();
-        System.out.printf("pid: %d%n", pid);
+        System.out.printf("Il mio pid e': %d%n", pid);
+
+        int numeroSegretessimo = (int) (Math.random() * 1000) + 1;
+        System.out.println("Ho estratto un numero da 1 a 1000");
 
         Scanner tastiera = new Scanner(System.in);
-        System.out.println("Inserisci un numero");
+        System.out.println("Indovina il numero inserendolo qui sotto");
         int numeroInserito = tastiera.nextInt();
         System.out.printf("Hai inserito il numero %d!%n", numeroInserito);
-        
+
+        if (numeroInserito == numeroSegretessimo) {
+            System.out.println("INCREDIBILE come hai fatto?? Hai VINTOhhh!");
+        } else {
+            System.out.println("Ritenta!");
+        }
+
         tastiera.close();
     }
 }
